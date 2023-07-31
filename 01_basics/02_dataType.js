fec76b9c30bd5563c6ae146209f592bbe8dce625
Mohammad_Zeeshan <57528872+MohammadZeeshan01@users.noreply.github.com>
2023-07-31 12:53:16 +0000

    two file added

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")//we are using nodejs not browser
+
+// code readability should be high
+
+let name = "Zeeshan";
+let age = 18;
+isLogdIn = false;
+
+// number => 2 to power 53
+// bigint => For big number
+// string => ""
+// boolean => true/false
+// null => standalone value //typeof null is object
+// undefined => value is not assign
+// symbol => unique
+
+// object
