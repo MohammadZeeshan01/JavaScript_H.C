dac562d02a705f76fab04386269f60f7f7373379
Mohammad_Zeeshan <57528872+MohammadZeeshan01@users.noreply.github.com>
2023-08-08 09:39:44 +0000

    added more files

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+//Primitive data type 
+
+// 7 type
+//String, Number, Boolearn, null, undefined, symbol, BigInt.
+
+// Reference (Non Primitive) data type
+
+// Array, Objects, Function
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(anotherId === id); 
