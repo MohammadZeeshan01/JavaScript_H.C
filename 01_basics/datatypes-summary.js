d237cd91a087ae711666a829e847af3883f9c1a6
Mohammad_Zeeshan <57528872+MohammadZeeshan01@users.noreply.github.com>
2023-08-11 09:13:57 +0000

    added new files

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,12 +1,37 @@
-//Primitive data type 
+//  Primitive
 
-// 7 type
-//String, Number, Boolearn, null, undefined, symbol, BigInt.
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-// Reference (Non Primitive) data type
+const score = 100
+const scoreValue = 100.3
 
-// Array, Objects, Function
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(anotherId === id); 
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
